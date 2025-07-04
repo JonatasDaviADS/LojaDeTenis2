4c23e3bbee99b06a9d42c0f4a817c270592810e7
Jonatas <jonatasdavi.ads@gmail.com>
2025-07-04 18:51:48 -0300

    feat: Melhorias na classe ProdPedi. Colocado o required em ProdutoId e PedidoId. Range + o DataTaype para evitar valores inválidos.

--- LojaDeTenis/Models/ProdPedi.cs
+++ LojaDeTenis/Models/ProdPedi.cs
@@ -7,16 +7,22 @@ namespace LojaDeTenis.Models
         public int Id { get; set; }
 
         // Relação com Produto
+        [Required(ErrorMessage = "Produto obrigatório")]
         public int ProdutoId { get; set; }
         public Produto? Produto { get; set; }
 
         // Relação com Pedido
+
+        [Required(ErrorMessage = "Pedido obrigatório")]
         public int PedidoId { get; set; }
         public Pedido? Pedido { get; set; }
 
         [Range(1, int.MaxValue, ErrorMessage = "Quantidade mínima é 1")]
         public int Quantidade { get; set; }
 
+
+        [Range(0.01, 10000.00, ErrorMessage = "Preço deve ser entre R$0,01 e R$10.000,00")]
+        [DataType(DataType.Currency)]
         public decimal PrecoUnitario { get; set; }
     }
 }

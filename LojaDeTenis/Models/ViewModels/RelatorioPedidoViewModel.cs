152a39dab762c21aab3391373d7d22d1d3bfb533
Jonatas <jonatasdavi.ads@gmail.com>
2025-07-04 14:10:21 -0300

    feat: adicona view e lógica do relatório de pedidos

---
+++ LojaDeTenis/Models/ViewModels/RelatorioPedidoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LojaDeTenis.Models.ViewModels
+{
+    public class RelatorioPedidoViewModel
+    {
+        public DateTime DataPedido { get; set; }
+        public string NomeCliente { get; set; }
+        public int QuantidadeProdutos { get; set; }
+        public decimal ValorTotal { get; set; }
+    }
+}

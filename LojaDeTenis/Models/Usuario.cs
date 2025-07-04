c16a3ff8573082dd6d55da70b943019915a0507c
Jonatas <jonatasdavi.ads@gmail.com>
2025-07-04 17:11:46 -0300

    feat: validações com DataAnnotations na classe Usuario

--- LojaDeTenis/Models/Usuario.cs
+++ LojaDeTenis/Models/Usuario.cs
@@ -1,11 +1,26 @@
-﻿namespace LojaDeTenis.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LojaDeTenis.Models
 {
     public class Usuario
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        [StringLength(100, ErrorMessage = "O nome deve ter no máximo 100 caracteres")]
         public string? Nome { get; set; }
+
+
+        [Required(ErrorMessage = "O e-mail é obrigatório")]
+        [EmailAddress(ErrorMessage = "Formato de e-mail inválido")]
         public string? Email { get; set; }
+
+
+        [Required(ErrorMessage = "A senha é obrigatória")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "A senha deve ter entre 6 e 100 caracteres")]
         public string? SenhaHash { get; set; }
+
+
         public bool IsAdmin { get; set; }
     }
 }
